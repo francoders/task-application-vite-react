a702443adba2bd9fd92ed6e1aca779e10739e5e1
Francisco Corvalan <francisco22corvalan@gmail.com>
2022-06-21 01:33:22 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.scss";
+import { Button } from "react-bootstrap";
+
+export default function App() {
+	return (
+		<>
+			<h1>App</h1>
+			<Button className="btn">Bootstrap</Button>
+		</>
+	);
+}
