66fe53cb4792ee8d33c9366882175f1344748304
Francisco Corvalan <francisco22corvalan@gmail.com>
2022-06-23 23:33:13 -0400

    modificacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { Button } from "react-bootstrap";
 import { NavBar, TaskForm, ElModal } from "./components";
 
 export default function App() {
 	const [showModal, setShowModal] = useState(false);
+	const [num, setNum] = useState(0);
 
 	const openCloseModal = () => {
 		setShowModal(!showModal);
